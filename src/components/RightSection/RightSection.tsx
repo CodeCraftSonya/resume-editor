e83d9c69fd936776103c8fd5ac42378c7e67433a
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-07-02 17:24:42 +0300

    feat: add dnd in right section

--- src/components/RightSection/RightSection.tsx
+++ src/components/RightSection/RightSection.tsx
@@ -12,54 +12,66 @@ const RightSection = ({
     <div className={styles.previewWrapper}>
       <div className={styles.page}>
         <h3 className={styles.name}>{name}</h3>
-        {activeSections.includes('About') && about && (
-          <section className={styles.section}>
-            <h3>О себе</h3>
-            <p>{about}</p>
-          </section>
-        )}
-
-        {activeSections.includes('Education') && (
-          <section className={styles.section}>
-            <h3>Образование</h3>
-            {education.map((edu, idx) => (
-              <div key={idx} className={styles.item}>
-                <strong>{edu.institution}</strong>
-                <div>{edu.faculty}</div>
-                <em>{edu.level.label}</em>
-              </div>
-            ))}
-          </section>
-        )}
-
-        {activeSections.includes('Skills') && (
-          <section className={styles.section}>
-            <h3>Навыки</h3>
-            <div className={styles.skills}>
-              {skills.map((skill, idx) => (
-                <span key={idx} className={styles.skill}>
-                  {skill}
-                </span>
-              ))}
-            </div>
-          </section>
-        )}
-
-        {activeSections.includes('Sertificates') && (
-          <section className={styles.section}>
-            <h3>Сертификаты</h3>
-            <div className={styles.certWrapper}>
-              {certificates.map((cert, idx) => (
-                <div key={idx} className={styles.item}>
-                  <strong>{cert.course}</strong> — {cert.organization}
-                  {cert.date && (
-                    <div>{cert.date.toLocaleDateString('ru-RU')}</div>
-                  )}
-                </div>
-              ))}
-            </div>
-          </section>
-        )}
+        {activeSections.map((section) => {
+          switch (section) {
+            case 'About':
+              return (
+                about && (
+                  <section className={styles.section} key='About'>
+                    <h3>О себе</h3>
+                    <p>{about}</p>
+                  </section>
+                )
+              );
+            case 'Education':
+              return (
+                education.length > 0 && (
+                  <section className={styles.section} key='Education'>
+                    <h3>Образование</h3>
+                    {education.map((edu, idx) => (
+                      <div key={idx}>
+                        <strong>{edu.level}</strong>, {edu.institution}
+                      </div>
+                    ))}
+                  </section>
+                )
+              );
+            case 'Skills':
+              return (
+                skills.length > 0 && (
+                  <section className={styles.section} key='Skills'>
+                    <h3>Навыки</h3>
+                    <div className={styles.skills}>
+                      {skills.map((skill, idx) => (
+                        <span key={idx} className={styles.skill}>
+                          {skill}
+                        </span>
+                      ))}
+                    </div>
+                  </section>
+                )
+              );
+            case 'Sertificates':
+              return (
+                certificates.length > 0 && (
+                  <section className={styles.section} key='Sertificates'>
+                    <h3>Сертификаты</h3>
+                    {certificates.map((cert, idx) => (
+                      <div key={idx}>
+                        <strong>{cert.course}</strong> — {cert.organization}
+                        <br />
+                        {cert.date && (
+                          <div>{cert.date.toLocaleDateString('ru-RU')}</div>
+                        )}
+                      </div>
+                    ))}
+                  </section>
+                )
+              );
+            default:
+              return null;
+          }
+        })}
       </div>
     </div>
   );
